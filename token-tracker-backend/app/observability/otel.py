0642cdd883e7125bcd767f4069b08c6cf4f55329
Youssef Elfaramawy <yelfaram@gmail.com>
2025-06-01 22:53:53 -0400

    Connected OTP exporter to local Tempo instance

--- token-tracker-backend/app/observability/otel.py
+++ token-tracker-backend/app/observability/otel.py
@@ -1,7 +1,10 @@
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+tempo_endpoint = "http://tempo:4318/v1/traces"
 
 resource = Resource.create(attributes={
     SERVICE_NAME: "token-tracker-service"
@@ -9,8 +12,8 @@ resource = Resource.create(attributes={
 
 # create tracer engine
 tracerProvider = TracerProvider(resource=resource)
-# init span processor that will send batch of traces to an endpoint (for now use console)
-processor = BatchSpanProcessor(span_exporter=ConsoleSpanExporter())
+# init span processor that will send batch of traces to an endpoint
+processor = BatchSpanProcessor(span_exporter=OTLPSpanExporter(endpoint=tempo_endpoint))
 tracerProvider.add_span_processor(span_processor=processor)
 
 # hook everything up to enable tracing globally
